4a0cf58c369c00dc2e57ae0d257bf23e42e68ccd
Florent S <sandri822@hotmail.fr>
2023-04-16 17:25:08 +0200

    💄 Update UI button authentification

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,15 @@
 class HomeController < ApplicationController
-  def index; end
+  before_action :set_devise_resource, only: [:index]
+
+  def index
+    @resource ||= User.new
+  end
+
+  private
+
+  def set_devise_resource
+    @resource ||= User.new
+    @resource_name ||= :user
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
