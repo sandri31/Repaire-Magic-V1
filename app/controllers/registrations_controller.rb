81a67aeb2c1b8a5f1f7cd9bced6d69e2132589d2
Florent S <sandri822@hotmail.fr>
2023-04-17 11:17:59 +0200

    ✨ Add alart message Devise in french

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -20,7 +20,7 @@ class RegistrationsController < Devise::RegistrationsController
       respond_to do |format|
         format.html { render :new }
         format.turbo_stream do
-          flash.now[:alert] = resource.errors.full_messages.to_sentence
+          flash.now[:alert] = resource.errors.full_messages.each_with_index.map { |msg, index| "#{msg}#{' Et ' if index < resource.errors.count - 1}" }.join
           render turbo_stream: turbo_stream.replace(:flash_messages, partial: "partials/flash", locals: { flash: flash })
         end
       end
