2f2ea3ab1b95cccab8360b8f4c14f718b2a1ed70
Florent S <sandri822@hotmail.fr>
2023-04-17 10:36:08 +0200

    🔧 Update alert messages configuration Devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,29 @@
+class RegistrationsController < Devise::RegistrationsController
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_to do |format|
+        format.html { render :new }
+        format.turbo_stream do
+          flash.now[:alert] = resource.errors.full_messages.to_sentence
+          render turbo_stream: turbo_stream.replace(:flash_messages, partial: "partials/flash", locals: { flash: flash })
+        end
+      end
+    end
+  end
+end
