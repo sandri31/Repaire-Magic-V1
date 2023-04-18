8dfcabd57f894b83c9666b913ec03862acabd46d
Florent S <sandri822@hotmail.fr>
2023-04-18 19:22:02 +0200

    ✨ Introduice new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { sessions: 'sessions', registrations: 'registrations', passwords: 'users/passwords',
                             confirmations: 'users/confirmations' }
+
+  get 'about', to: 'static_pages#about'
+  get 'services', to: 'static_pages#services'
+  get 'contact', to: 'static_pages#contact'
+
   root 'home#index'
 end
