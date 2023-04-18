ec0b955988eda22b83e60857fba02687168adc40
Florent S <sandri822@hotmail.fr>
2023-04-18 09:00:38 +0200

    ♻️ Refactor code

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -6,15 +6,15 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     if resource.errors.empty?
       sign_in(resource) # Ajoutez cette ligne pour connecter l'utilisateur automatiquement
       set_flash_message(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      respond_with_navigational(resource) { redirect_to after_confirmation_path_for(resource_name, resource) }
     else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      respond_with_navigational(resource.errors, status: :unprocessable_entity) { render :new }
     end
   end
 
   protected
 
-  def after_confirmation_path_for(resource_name, resource)
+  def after_confirmation_path_for(_resource_name, _resource)
     root_path
   end
 end
