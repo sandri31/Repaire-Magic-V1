f75e29f90d5311fe414a1385639282325bc883be
Florent S <sandri822@hotmail.fr>
2023-04-19 21:18:39 +0200

    🔧 Fix bug authentification with github 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
+      user.email = auth.info.email || "#{auth.extra.raw_info.login}@example.com"
       user.password = Devise.friendly_token[0, 20]
       user.skip_confirmation!
       user.full_name = auth.info.name
