303ddc9c06d35562f116c04a8960db767cc67ea8
Florent S <sandri822@hotmail.fr>
2023-04-25 12:03:28 +0200

    🔒️ Add !user_signed_in services controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def about; end
 
-  def services; end
+  def services
+    !user_signed_in? && redirect_to(root_path, alert: 'Vous devez être connecté pour accéder à cette page')
+  end
 
   def contact; end
 end
